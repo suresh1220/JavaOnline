6901d606d0096042365b1f9d2f8cf3b2e8e87c6c
suresh1220 <suri.rsb@gmail.com>
2020-01-01 15:12:01 +0530

    modified test class

--- Comparator/src/com/comparator/test/Test.java
+++ Comparator/src/com/comparator/test/Test.java
@@ -1,8 +1,20 @@
 package com.comparator.test;
 
 public class Test {
+	private int id;
+	private String name;
+	private String address;
+
+	@Override
+	public String toString() {
+		return "Test [id=" + id + ", name=" + name + ", address=" + address + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
+	}
+
 	public static void main(String[] args)
 	{
+		Test t= new Test();
+		
 		System.out.println("This for test push from sts");
 	}
 
